fd73c10a11a7cb5dc30889be9873a12c42caaf6b
Harry <xuj10@rpi.edu>
2018-10-29 20:58:06 -0400

    Add some more things to MainMenu..Still learning this engine

---
+++ WreckIt/Assets/Scripts/MainMenuStartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuStartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ClickStartButton() {
+        // UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName("SampleScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
