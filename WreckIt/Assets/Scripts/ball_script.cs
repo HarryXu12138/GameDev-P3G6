003ff74dc3165706fde997eaa33584b36b71ccd8
Jeff Yang <www.speedway@hotmail.com>
2018-10-31 15:58:10 -0400

    added power bar and prompt player to pick the ball
    charge by pressing E, walk up to the ball and press SPACE to pick it up

--- WreckIt/Assets/Scripts/ball_script.cs
+++ WreckIt/Assets/Scripts/ball_script.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ball_script : MonoBehaviour {
 
@@ -13,12 +14,24 @@ public class ball_script : MonoBehaviour {
     private float angleX = 0.0f;
     private float angleY = 0.0f;
 
+    private bool pickable = false;
+
+    public Text pick_ball_text;
+    public GameObject da_pick_text_panel;
+
+    public Slider power_bar;
+    private bool power_up;
+
     private GameObject da_cam;
     private Rigidbody rb;
     //private static Rigidbody cam_rb;
     private bool thrown;
     // Use this for initialization
     void Start () {
+        da_pick_text_panel.SetActive(false);
+        power_up = true;
+        power_bar.value = 0;
+        pick_ball_text.text = "Press SPACE to pick up the ball";
         thrown = false;
         da_cam = GameObject.Find("Main Camera");
         transform.position = da_cam.transform.position + (new Vector3(0.5f, 0.0f, 1.0f));
@@ -31,12 +44,33 @@ public class ball_script : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Debug.Log(da_cam.GetComponent<Transform>().eulerAngles.y);
-        if (Input.GetKey(KeyCode.E) && !thrown){
+        if(!thrown && Input.GetKey(KeyCode.E))
+        {
+            //starts charging
+            if(power_up){
+                Debug.Log("power up");
+                power_bar.value += Time.deltaTime * throw_speed;
+                if(power_bar.value > 100){
+                    power_up = false;
+                }
+            }
+            else{
+                Debug.Log("power down");
+                power_bar.value -= Time.deltaTime * throw_speed;
+                if(power_bar.value < 0){
+                    power_up = true;
+                }
+            }
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.E) && !thrown){
             thrown = true;
             transform.SetParent(null);
             //throw the ball upon clicking the E button
             rb.useGravity = true;
-            Vector3 velocity = new Vector3(0, 0, throw_speed);
+            float tspd = power_bar.value * 1.3f;
+            Vector3 velocity = new Vector3(0, 0, tspd);
             velocity = Quaternion.Euler(da_cam.transform.eulerAngles.x, da_cam.transform.eulerAngles.y, 0) * velocity;
             //Console.WriteLine()
             //kinda odd that I can't use *= on this
@@ -44,24 +78,45 @@ public class ball_script : MonoBehaviour {
             rb.AddForce(velocity);
             
         }
-        if(Input.GetKey(KeyCode.Space) && thrown)
+
+        if (thrown)
         {
-            pick();
+            power_bar.value = 0;
+            if ((Math.Abs(transform.position.x - da_cam.transform.position.x) < 1) &&
+                (Math.Abs(transform.position.z - da_cam.transform.position.z) < 1) && pickable)
+            {
+
+                da_pick_text_panel.SetActive(true);
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    pick();
+                }
+            }
         }
 
 
+
     }
-    
+
     void pick()
     {
+        //pick_ball_text.text = "";
+        da_pick_text_panel.SetActive(false);
         thrown = false;
-        
+        pickable = false;
         transform.position = transform.position = da_cam.transform.position + Quaternion.Euler(da_cam.transform.eulerAngles.x, da_cam.transform.eulerAngles.y, 0) * (new Vector3(0.5f, 0.0f, 1.0f));
         transform.SetParent(da_cam.transform);
-        
+
         rb.velocity = new Vector3(0, 0, 0);
         rb.angularVelocity = new Vector3(0, 0, 0);
         rb.useGravity = false;
     }
-    
+
+    private void OnCollisionStay(Collision c)
+    {
+        if(rb.velocity.magnitude < 1 &&
+           rb.angularVelocity.magnitude < 1){
+            pickable = true;
+        }
+    }
 }
