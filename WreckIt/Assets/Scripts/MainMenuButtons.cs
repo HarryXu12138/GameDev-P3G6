23f7f970d25c6c1db0eef1e84742e47bba16ab67
Harry <xuj10@rpi.edu>
2018-10-31 14:01:32 -0400

    Add return buttons

---
+++ WreckIt/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour {
+    public Button StartButton;
+    public Button HelpButton;
+
+	// Use this for initialization
+	void Start () {
+        StartButton.onClick.AddListener(ClickStartButton);
+        HelpButton.onClick.AddListener(ClickHelpButton);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ClickHelpButton()
+    {
+        // UnityEngine.SceneManagement.SceneManager.LoadScene("");
+        Debug.Log("Enter Help Page.");
+    }
+
+    void ClickStartButton() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
