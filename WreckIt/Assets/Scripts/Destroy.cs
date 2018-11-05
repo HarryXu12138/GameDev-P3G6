b3450fa9f2394a9ba515de9c39fba3845e2a7625
Kevin Chen <cheny43@rpi.edu>
2018-11-05 17:39:22 -0500

    Fixed the bugs in Destroy

--- WreckIt/Assets/Scripts/Destroy.cs
+++ WreckIt/Assets/Scripts/Destroy.cs
@@ -41,7 +41,7 @@ public class Destroy : MonoBehaviour {
             //GameObject temp = GameObject.Instantiate<GameObject>(debriPrefab);
             //temp.transform.position = transform.position;
             Vector3 force = collision.impulse;
-            force.Scale(new Vector3(-1f*impulseMagnitude, -1f * impulseMagnitude, -1f * impulseMagnitude));
+            //force.Scale(new Vector3(-1f*impulseMagnitude, -1f * impulseMagnitude, -1f * impulseMagnitude));
             //force = Random.onUnitSphere * collision.impulse.magnitude;
             print(force);
             if (Mathf.Abs(force.x + force.y + force.z) > breakpoint)
@@ -82,26 +82,111 @@ public class Destroy : MonoBehaviour {
             //check the direction
             Vector3 tempF = force;
             //print(Mathf.Abs(tempF.x - 0f) < 0.01f);
-            if (Mathf.Abs(tempF.x - 0f) < 0.03f)
+            float maxPower = Mathf.Max(tempF.x, tempF.y, tempF.z);
+            if (maxPower == tempF.x)
             {
                 //print("work");
-                tempF.x += Random.Range(-spread + shift, spread + shift);
+                if (Mathf.Max(tempF.z, tempF.y) == tempF.z)
+                {
+                    tempF.z += Random.Range(-spread + tempF.z*10f, spread + tempF.z*10f);
+                    tempF.y += Random.Range(-spread, spread);
+                    if (tempF.y < -10.0f)
+                    {
+                        tempF.y = -tempF.y;
+                        if (tempF.y < 2.0f)
+                        {
+                            tempF.y = 2.0f;
+                        }
+                        if (tempF.y > tempF.z)
+                        {
+                            tempF.y = tempF.z;
+                        }
+                    }
+                }
+                else
+                {
+                    tempF.z += Random.Range(-spread , spread);
+                    tempF.y += Random.Range(-spread + tempF.y*10f, spread + tempF.y*10f);
+                    if (tempF.y<-10.0f){
+                        tempF.y = -tempF.y;
+                        if (tempF.y < 2.0f){
+                            tempF.y = 2.0f;
+                        }
+                        if (tempF.y > tempF.z)
+                        {
+                            tempF.y = tempF.z;
+                        }
+                    }
+                }
+                
+
+
             }
-            if (Mathf.Abs(tempF.y - 0f) < 0.03f)
+            else if (maxPower == tempF.z)
             {
-                tempF.y += Random.Range(-spread + shift, spread + shift);
-                if (tempF.y<0){
-                    tempF.y = -tempF.y;
-                    if (tempF.y < 2.0f){
-                        tempF.y = 2.0f;
-                    } 
+                if (Mathf.Max(tempF.x,tempF.y) == tempF.x)
+                {
+                    tempF.x += Random.Range(-spread + tempF.x*shift, spread + tempF.x*shift);
+                    tempF.y += Random.Range(-spread , spread);
+                    if (tempF.y < -10.0f)
+                    {
+                        tempF.y = -tempF.y;
+                        if (tempF.y < 2.0f)
+                        {
+                            tempF.y = 2.0f;
+                        }
+                        if (tempF.y > tempF.z)
+                        {
+                            tempF.y = tempF.z;
+                        }
+                    }
+                }
+                else
+                {
+                    tempF.x += Random.Range(-spread , spread );
+                    tempF.y += Random.Range(-spread + tempF.y*shift, spread + tempF.y*shift);
+                    if (tempF.y < -10.0f)
+                    {
+                        tempF.y = -tempF.y;
+                        if (tempF.y < 2.0f)
+                        {
+                            tempF.y = 2.0f;
+                        }
+                        if (tempF.y > tempF.z)
+                        {
+                            tempF.y = tempF.z;
+                        }
+                    }
+                }
+                
+            }
+            else
+            {
+                if(Mathf.Max(tempF.x, tempF.z) == tempF.x) {
+                    tempF.z += Random.Range(-spread , spread );
+                    tempF.x += Random.Range(-spread + tempF.x*shift, spread + tempF.x*shift);
+                }
+                else {
+                    tempF.z += Random.Range(-spread + tempF.z * shift, spread + tempF.z * shift);
+                    tempF.x += Random.Range(-spread , spread );
+                }
+                
+            }
+            
+            if (tempF.y < -10.0f)
+            {
+                tempF.y = -tempF.y;
+                if (tempF.y < 2.0f)
+                {
+                    tempF.y = 2.0f;
+                }
+                if (tempF.y > maxPower)
+                {
+                    tempF.y = maxPower;
                 }
             }
-            if (Mathf.Abs(tempF.z - 0f) < 0.03f)
-            {
-                tempF.z += Random.Range(-spread + shift, spread + shift);
-            }
-            //print(tempF);
+            tempF.Scale(new Vector3(1f * impulseMagnitude, 1f * impulseMagnitude, 1f * impulseMagnitude));
+            print(tempF);
             addForceToPrefab(temp, tempF);
             Destroy(temp, prefabDestroyTime);
             counter--;
