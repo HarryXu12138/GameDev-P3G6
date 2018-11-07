3f212063d14f3508be4b0c348deb49c6ecc4825b
IceAuraDragon <32280166+IceAuraDragon@users.noreply.github.com>
2018-11-06 22:51:39 -0500

    Add files via upload

---
+++ WreckIt/Assets/Scripts/RunAway.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAway : MonoBehaviour {
+    public Transform ex;
+    public float check;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vec = new Vector3(1, (Mathf.Abs(Mathf.Sin(3.5f*Time.time))+1)/2, 1);
+        check = (Mathf.Abs(Mathf.Sin(3.5f * Time.time)) + 1) / 2;
+        ex.transform.localScale = vec;
+    }
+}
